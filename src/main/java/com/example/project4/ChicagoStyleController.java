cfab8f0b9a6b3c71e1984a89a23ce4b71f7072e6
Reiya Dave <reiya.dave@gmail.com>
2022-11-20 18:15:46 -0500

    starter stuff-reiya

--- src/main/java/com/example/project4/ChicagoStyleController.java
+++ src/main/java/com/example/project4/ChicagoStyleController.java
@@ -1,62 +1,57 @@
 package com.example.project4;
 
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 
-public class ChicagoStyleController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ChicagoStyleController implements Intializable{
 
     @FXML
-    private ComboBox<String> addToppings1;
+    private ComboBox<String> addToppings1 = new ComboBox <String> ();
 
     @FXML
-    private ComboBox<String> flavorBox1;
-    public void initialize() {
-        flavorBox1.setItems(FXCollections.observableArrayList("Deluxe", "BBQ Chicken", "Meatzza", "Build Your Own"));
-    }
+    private ComboBox<String> flavorBox1 = new ComboBox <String> ();
 
     @FXML
     private ImageView imageView1;
 
     @FXML
-    private ComboBox<String> sizeBox1;
+    private ComboBox<String> sizeBox1 = new ComboBox <String> ();
 
     @FXML
-    private ComboBox<String> toppingsBox1;
+    private ComboBox<String> toppingsBox1 = new ComboBox <String> ();
 
     @FXML
     private TextArea totalBox1;
 
 
     @FXML
-    public void selectFlavor(MouseEvent event) {
-        flavorBox1.setItems(FXCollections.observableArrayList("Deluxe", "BBQ Chicken", "Meatzza", "Build Your Own"));
-        //flavorBox1.getItems().add("whatever");
-//        flavorBox1.getItems().addAll(){
-//                    "Deluxe",
-//                    "BBQ Chicken",
-//                    "Meatzza",
-//                    "Build Your Own"
-//        }
-    }
-
-    @FXML
-    void selectMoreToppings(ActionEvent event) {
+    void selectFlavor(ActionEvent event) {
+        String flavorString = flavorBox1.getSelectionModel().getSelectedItem().toString();
 
     }
 
     @FXML
-    void selectSize(ActionEvent event) {
-
+    void selectSize(ActionEvent event){
+        String sizeString = sizeBox1.getSelectionModel().getSelectedItem().toString();
     }
 
-    @FXML
-    void selectToppings(ActionEvent event) {
 
+    @Override
+    public void initialize(URL url, ResourceBundle rb){
+        ObservableList<String> flavorList= FXCollections.observableArrayList("Deluxe", "BBQ", "Meatzza", "Build Your Own");
+        flavorBox1.setItems(flavorList);
+
+        ObservableList<String> size = FXCollections.observableArrayList("small", "medium", "large");
+        sizeBox1.setItems(size);
     }
+
 
 }
