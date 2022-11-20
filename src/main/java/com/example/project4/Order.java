b3af70e3330d681ff30d2c89bab6c512d848cdc4
Reiya Dave <reiya.dave@gmail.com>
2022-11-20 16:36:45 -0500

    starter stuff-reiya

--- src/main/java/com/example/project4/Order.java
+++ src/main/java/com/example/project4/Order.java
@@ -1,11 +1,56 @@
 package com.example.project4;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Order implements Customizable{
-    public boolean add(Object obj){
+    public int orderNumber;
+    public double orderCost;
+    //make arraylist of pizzas?
+    public ArrayList<Pizza> pizzaOrder;
+    //add and remove pizzas from arraylist using add() and remove()
 
+    public Order(){
+        Random rand = new Random();
+        orderNumber = rand.nextInt();
+    }
+
+    public boolean add(Object obj){
+        //add pizza to order?
+        //obj is the pizza, add it to the order
+        //somehow typecast obj to pizza type
+        Pizza temp = (Pizza) obj;
+        if (pizzaOrder.add(temp) == true){
+            return true;
+        }
+        return false;
     }
 
     public boolean remove(Object obj){
+        Pizza temp = (Pizza) obj;
+        if (pizzaOrder.remove(temp) == true){
+            return true;
+        }
+        return false;
+    }
 
+    public String orderToString() {
+        //order number plus pizza
+        String result = "";
+        result += "Order Number: " + orderNumber + "\n";
+        for (int i = 0; i < pizzaOrder.size(); i++){
+            result += pizzaOrder.get(i).toString() + "; ";
+        }
+        return result;
+    }
+
+    //method for cost of order
+    public double addCost(Double cost){
+        orderCost += cost;
+        return orderCost;
+    }
+
+    public double removeCost(Double cost){
+        orderCost -= cost;
+        return orderCost;
     }
 }
--- src/main/java/com/example/project4/StoreOrders.java
+++ src/main/java/com/example/project4/StoreOrders.java
@@ -1,11 +1,56 @@
 package com.example.project4;
 
-public class StoreOrders implements Customizable{
-    public boolean add(Object obj){
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
+import java.util.ArrayList;
 
+public class StoreOrders implements Customizable{
+
+    //arraylist of order type
+    public ArrayList<Order> orderList;
+    public double totalCost;
+    public boolean add(Object obj){
+        //add order type to arraylist
+        Order temp = (Order) obj;
+        if (orderList.add(temp) == true){
+            return true;
+        }
+        return false;
     }
 
     public boolean remove(Object obj){
+        //remove order type from arraylist
+        Order temp = (Order) obj;
+        if (orderList.remove(temp) == true){
+            return true;
+        }
+        return false;
+    }
 
+
+    //method for total cost of order
+    public double addTotalCost(Double cost){
+        totalCost += cost;
+        return totalCost;
+    }
+    public double removeTotalCost(Double cost){
+        totalCost -= cost;
+        return totalCost;
+    }
+
+    //method to export orders to a text file
+    public void export(){
+        try {
+            FileWriter myWriter = new FileWriter("filename.txt");
+            //here write contents of arraylist
+            for (int i = 0; i < orderList.size(); i++){
+                myWriter.write(orderList.get(i).orderToString() + "\n");
+            }
+            myWriter.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
