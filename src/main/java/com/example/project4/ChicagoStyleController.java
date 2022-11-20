d5e3da531f8f41f955a1f28596e0edb87b7fb38b
Ifrah Sajjad <67663463+isajjad1246@users.noreply.github.com>
2022-11-20 16:37:13 -0500

    ifrah

--- src/main/java/com/example/project4/ChicagoStyleController.java
+++ src/main/java/com/example/project4/ChicagoStyleController.java
@@ -1,24 +1,62 @@
 package com.example.project4;
+
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.ListView;
-import javafx.scene.layout.HBox;
-import javafx.stage.Stage;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
-import java.net.URL;
-
-public class ChicagoStyleController{
+public class ChicagoStyleController {
 
     @FXML
-    ObservableList<String> flavors = FXCollections.observableArrayList("Deluxe", "BBQ Chicken", "Meatzza", "Build Your Own");
-    ListView pizzaFlavors = new ListView ();
-//    pizzaFlavors.getItems().add()
+    private ComboBox<String> addToppings1;
 
+    @FXML
+    private ComboBox<String> flavorBox1;
+
+    @FXML
+    private ImageView imageView1;
+
+    @FXML
+    private ComboBox<String> sizeBox1;
+
+    @FXML
+    private ComboBox<String> toppingsBox1;
+
+    @FXML
+    private TextArea totalBox1;
+
+
+    @FXML
+    void selectFlavor(MouseEvent event) {
+       // flavorBox1.setItems(FXCollections.observableArrayList("Deluxe", "BBQ Chicken", "Meatzza", "Build Your Own"));
+        flavorBox1.getItems().add("whatever");
+//        flavorBox1.getItems().addAll(){
+//                    "Deluxe",
+//                    "BBQ Chicken",
+//                    "Meatzza",
+//                    "Build Your Own"
+//        }
+
+
+
+    }
+
+    @FXML
+    void selectMoreToppings(ActionEvent event) {
+
+    }
+
+    @FXML
+    void selectSize(ActionEvent event) {
+
+    }
+
+    @FXML
+    void selectToppings(ActionEvent event) {
+
+    }
 
 }
