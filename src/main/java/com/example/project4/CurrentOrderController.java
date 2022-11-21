b35e2b30950be3d5c8ded73bfc82999985559ffd
Reiya Dave <reiya.dave@gmail.com>
2022-11-20 21:26:17 -0500

    starter stuff-reiya

--- src/main/java/com/example/project4/CurrentOrderController.java
+++ src/main/java/com/example/project4/CurrentOrderController.java
@@ -1,5 +1,41 @@
 package com.example.project4;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+
 public class CurrentOrderController {
 
+    @FXML
+    private TextField orderNum;
+
+    @FXML
+    private ListView listViewOrders;
+
+    @FXML
+    private TextField subtotal;
+
+    @FXML
+    private TextField salesTax;
+
+    @FXML
+    private TextField orderTotal;
+
+    @FXML
+    private Button removePizza;
+
+    @FXML
+    private Button placeOrder;
+
+    @FXML
+    private Button clearOrder;
+
+    //methods for actions and stuff
+    @FXML
+    public void displayOrderNum(){
+
+    }
 }
