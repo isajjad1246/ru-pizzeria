461d0ff4a5860707c6514501d6e341943ce88658
Reiya Dave <reiya.dave@gmail.com>
2022-11-20 19:12:56 -0500

    starter stuff-reiya

--- src/main/java/com/example/project4/MainController.java
+++ src/main/java/com/example/project4/MainController.java
@@ -2,6 +2,7 @@ package com.example.project4;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 
 public class MainController {
     @FXML
@@ -10,5 +11,16 @@ public class MainController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    private RadioButton chicagoStyleButton;
+
+    @FXML
+    private RadioButton nyStyleButton;
+
+    public void chicagoButtonClick(){
+        //go to chicagostyle view
+
     }
 }
